2be851db0e0471b0c3c4d48edceb28ead5a02a69
James Seaton <seatonj2@users.noreply.github.com>
2016-10-27 15:01:38 -0400

    added function sum which adds everynumber from 1 to, and including, n

--- practice.py
+++ practice.py
@@ -1,7 +1,15 @@
 #practice file
+def sum(n):
+    "adds numbers from 1 to, and including, n"
+    result = 0
+    for i in range(1,n+1):
+        result = result + i
+    return result
+
 x = 10
 y = 20
 z = x * y
 
 print (x ,"+",y ,"=",z)
 
+print ("Sum of 1 to",x,"=",sum(x))
